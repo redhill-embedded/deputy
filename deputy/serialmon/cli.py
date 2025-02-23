2f99e935704e2c67b485801afbcae54b56e4164e
Adrian Rothenbuhler <adrian@redhill-embedded.com>
2025-02-22 17:07:24 -0700

    Serial monitor fixes

--- deputy/serialmon/cli.py
+++ deputy/serialmon/cli.py
@@ -1,12 +1,18 @@
 import argparse
+import colorama
 from colorama import Fore, Style
+import os
 import sys
 import traceback
 
 from deputy import __version__
 from deputy.serialmon.serialmon import SerialPort
+from deputy.serialmon.term import Term
+
 
 def serialmon_cli_print_port_list(verbose, test_availability):
+    if os.name == "nt":
+        colorama.init(convert=True)
     port_list = SerialPort.get_port_list()
     if not port_list:
         print("No serial ports available.")
@@ -27,14 +33,39 @@ def serialmon_cli_print_port_list(verbose, test_availability):
             print(f"\r\tDesc: {port.description}\n\r\tHWID: {port.hwid}")
 
 
-def serialmon_cli_open_port():
-    pass
+def serialmon_cli_open_port(port: str, serialnumber: str, baudrate: int):
+    port_list = SerialPort.get_port_list()
+    p_path = None
+
+    if port is not None:
+        # Try to get the port path from the port string
+        if port.isdecimal():
+            try:
+                p_path = SerialPort.get_port_path_from_ID(int(port) - 1)
+            except Exception as e:
+                print(f"ERROR: {port} is not a valid serial port ID ({e})")
+        else:
+            try:
+                p_path = SerialPort.validate_port_string(port)
+            except Exception as e:
+                print(f"ERROR: {e}")
+    elif serialnumber is not None:
+        # Try to get the port path from the serial number
+        try:
+            p_path = SerialPort.get_port_path_from_serialnumber(serialnumber)
+        except Exception as e:
+            print("ERROR: {e}")
+
+    if p_path is not None:
+        t = Term(p_path, baudrate)
+        t.start()
+
 
 def cli(argv):
     parser = argparse.ArgumentParser(description="Serial monitor CLI.")
     parser.add_argument('--version', action='version', version=__version__,
                                                 help="Print package version")
-    parser.add_argument('-S', '--serial', help='Serial number to search for')
+    parser.add_argument('-s', '--serial', help='Serial number to search for')
     parser.add_argument('-p', '--port', help="Serial port path or number")
     parser.add_argument('--baud', type=int, default=115200,
                                                 help="Baud rate (default: 115200)")
@@ -45,13 +76,14 @@ def cli(argv):
 
     args, remaining_args = parser.parse_known_args(argv)
 
-    if not args.port or args.serial:
+    if not args.port and not args.serial:
         # If no port or serial was provided, then we simply print the list of serial ports.
         serialmon_cli_print_port_list(args.verbose, args.test)
         return 0
     else:
         # Try to open the serial port
-        return serialmon_cli_open_port()
+        return serialmon_cli_open_port(args.port, args.serial, args.baud)
+
 
 def main(argv=None):
     """Serialmon CLI Main entry point"""

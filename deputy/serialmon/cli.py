6d010a2456414976fd27f3f24e266ae1672615aa
Adrian Rothenbuhler <adrian@redhill-embedded.com>
2025-03-01 14:45:18 -0700

    Add serial terminal handling options

--- deputy/serialmon/cli.py
+++ deputy/serialmon/cli.py
@@ -10,7 +10,7 @@ from deputy.serialmon.serialmon import SerialPort
 from deputy.serialmon.term import Term
 
 
-def serialmon_cli_print_port_list(verbose, test_availability):
+def serialmon_cli_print_port_list(verbose, check_availability):
     if os.name == "nt":
         colorama.init(convert=True)
     port_list = SerialPort.get_port_list()
@@ -20,7 +20,7 @@ def serialmon_cli_print_port_list(verbose, test_availability):
     print("Available serial ports:")
     for i, port in enumerate(port_list):
         dev_str = f"{i+1}. {port.path}"
-        if test_availability:
+        if check_availability:
             print(dev_str, end='')
             if port.is_available():
                 print(Fore.GREEN + "\tAvailable")
@@ -33,7 +33,7 @@ def serialmon_cli_print_port_list(verbose, test_availability):
             print(f"\r\tDesc: {port.description}\n\r\tHWID: {port.hwid}")
 
 
-def serialmon_cli_open_port(port: str, serialnumber: str, baudrate: int):
+def serialmon_cli_open_port(port: str, serialnumber: str, baudrate: int, config: str, term: str):
     port_list = SerialPort.get_port_list()
     p_path = None
 
@@ -57,32 +57,53 @@ def serialmon_cli_open_port(port: str, serialnumber: str, baudrate: int):
             print("ERROR: {e}")
 
     if p_path is not None:
-        t = Term(p_path, baudrate)
-        t.start()
+        try:
+            t = Term(p_path, baudrate, config, term)
+        except Exception as e:
+            print(e)
+            return -1
+        else:
+            t.start()
 
 
 def cli(argv):
     parser = argparse.ArgumentParser(description="Serial monitor CLI.")
+    parser.add_argument("command", nargs="?", choices=["listports", "open", "listterm"], help="Command to execute")
+    parser.add_argument("term", nargs="?", help="Terminal program name (for 'open')")
     parser.add_argument('--version', action='version', version=__version__,
                                                 help="Print package version")
     parser.add_argument('-s', '--serial', help='Serial number to search for')
     parser.add_argument('-p', '--port', help="Serial port path or number")
-    parser.add_argument('--baud', type=int, default=115200,
+    parser.add_argument('-b', '--baud', type=int, default=115200,
                                                 help="Baud rate (default: 115200)")
+    parser.add_argument('-c', '--config', default="8N1", help="Port configuration (default: 8N1)")
     parser.add_argument('-v', '--verbose', action='store_true',
                                                 help="Print more information")
     parser.add_argument('-t', '--test', action='store_true',
-                                                help="Test if serial port is available")
+                                                help="Check if serial port is available")
 
     args, remaining_args = parser.parse_known_args(argv)
 
-    if not args.port and not args.serial:
-        # If no port or serial was provided, then we simply print the list of serial ports.
+    command = args.command or "listports"
+
+    if command == "listports":
+        # Print the list of serial ports.
         serialmon_cli_print_port_list(args.verbose, args.test)
         return 0
-    else:
+
+    elif command == "listterm":
+        # Print the list of available serial terminal programs
+        term_list = Term.term_list()
+        for term, path in term_list.items():
+            if args.verbose:
+                print(f"{term}: {path}")
+            else:
+                print(f"{term}")
+        return 0
+
+    elif command == "open":
         # Try to open the serial port
-        return serialmon_cli_open_port(args.port, args.serial, args.baud)
+        return serialmon_cli_open_port(args.port, args.serial, args.baud, args.config, args.term)
 
 
 def main(argv=None):
